b7938941de3c2f85b4f401df13505bd2603266ad
aravinth-sivarasa <aravinth.sivarasa@gmail.com>
2024-12-25 10:16:06 +0100

    added config server

---
+++ config/src/main/java/io/pow/config/PowConfigApplication.java
@@ -0,0 +1,13 @@
+package io.pow.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowConfigApplication.class, args);
+	}
+
+}
