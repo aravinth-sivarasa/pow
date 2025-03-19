b85d078c6e6a589aa1db776b17e61f93d4b5c37f
aravinth-sivarasa <aravinth.sivarasa@gmail.com>
2025-03-19 20:15:45 +0100

    removed unwanted imports

--- kafka/src/main/java/io/pow/kafka/KafkaConfig.java
+++ kafka/src/main/java/io/pow/kafka/KafkaConfig.java
@@ -1,20 +1,7 @@
 package io.pow.kafka;
 
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-
 
 @Configuration
 public class KafkaConfig {
