688f0af83d5df90b69e583ebe9d778dd051bc972
aravinth-sivarasa <aravinth.sivarasa@gmail.com>
2025-03-30 14:00:29 +0200

    Added SequenceGenerator

--- backend/src/main/java/io/pow/backend/product/Product.java
+++ backend/src/main/java/io/pow/backend/product/Product.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
 @Data
@@ -11,7 +12,8 @@ import lombok.Data;
 public class Product {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_seq")
+    @SequenceGenerator(name = "product_seq", sequenceName = "product_sequence", allocationSize = 1)
     private Long id;
     private String code;
     private String description;
